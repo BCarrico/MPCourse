4b93be17b5fc920b0cbf8f3c7300ea16584be164
bradley carrico <BCarrico89@gmail.com>
2024-07-23 17:10:19 -0700

    Weapon Class

---
+++ Source/Blaster/Private/Weapon/Weapon.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/Weapon.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+	WeaponMesh->SetupAttachment(RootComponent);
+	SetRootComponent(WeaponMesh);
+	WeaponMesh->SetCollisionResponseToAllChannels(ECR_Block);
+	WeaponMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>("AreaSphere");
+	AreaSphere->SetupAttachment(RootComponent);
+	AreaSphere->SetCollisionResponseToChannels(ECollisionResponse::ECR_Ignore);
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::Type::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	}
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
