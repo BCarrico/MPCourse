7dd52afd4c67075b07b367773d979dc026f53d2e
bradley carrico <BCarrico89@gmail.com>
2024-08-17 12:50:56 -0700

    Match State

--- Source/Blaster/Private/Game/BlasterGameMode.cpp
+++ Source/Blaster/Private/Game/BlasterGameMode.cpp
@@ -7,6 +7,25 @@
 #include "Kismet/GameplayStatics.h"
 #include "PlayerState/BlasterPlayerState.h"
 
+ABlasterGameMode::ABlasterGameMode()
+{
+	bDelayedStart = true; // Stay in waiting to start state, default pawn spawned and can fly around
+}
+
+void ABlasterGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (MatchState == MatchState::WaitingToStart)
+	{
+		CountdownTime = WarmUpTime - GetWorld()->GetTimeSeconds() + LevelStartingTime;
+		if (CountdownTime <= 0.f)
+		{
+			StartMatch();
+		}
+	}
+}
+
 void ABlasterGameMode::PlayerEliminated(ABlasterCharacter* EliminatedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController)
 {
 	ABlasterPlayerState* AttackerPlayerState = AttackerController ? Cast<ABlasterPlayerState>(AttackerController->PlayerState) : nullptr;
@@ -40,4 +59,11 @@ void ABlasterGameMode::RequestRespawn(ACharacter* ElimintedCharacter, AControlle
 		int32 Selection = FMath::RandRange(0, PlayerStarts.Num()-1);
 		RestartPlayerAtPlayerStart(EliminatedController, PlayerStarts[Selection]);
 	}
+}
+
+void ABlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LevelStartingTime = GetWorld()->GetTimeSeconds();
 }
--- Source/Blaster/Public/Game/BlasterGameMode.h
+++ Source/Blaster/Public/Game/BlasterGameMode.h
@@ -18,6 +18,18 @@ class BLASTER_API ABlasterGameMode : public AGameMode
 	GENERATED_BODY()
 
 public:
+	ABlasterGameMode();
+	virtual void Tick(float DeltaSeconds) override;
 	virtual void PlayerEliminated(ABlasterCharacter* EliminatedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController);
 	virtual void RequestRespawn(ACharacter* ElimintedCharacter, AController* EliminatedController);
+
+	UPROPERTY(EditDefaultsOnly)
+	float WarmUpTime = 10.f;
+
+	float LevelStartingTime = 0.f;
+
+protected:
+	virtual void BeginPlay() override;
+private:
+	float CountdownTime = 0.f;
 };
